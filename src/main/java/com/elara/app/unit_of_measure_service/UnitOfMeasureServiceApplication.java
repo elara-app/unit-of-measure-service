9b86fbd6364531c856763c6d4d980bca033637ab
Julian Bermudez <julianbetov@gmail.com>
2025-07-05 18:16:36 -0500

    initialize unit of measure service with Maven setup and basic application structure

---
+++ src/main/java/com/elara/app/unit_of_measure_service/UnitOfMeasureServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elara.app.unit_of_measure_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitOfMeasureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitOfMeasureServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elara/app/unit_of_measure_service/UnitOfMeasureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elara.app.unit_of_measure_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitOfMeasureServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
