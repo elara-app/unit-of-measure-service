dedbb82714d94d9f7af34ad346cfc92a27020a1c
Andrés Bermúdez <91708539+andresbetov@users.noreply.github.com>
2025-07-05 19:11:14 -0500

    feat(model): add UomStatus entity with validation and default values

---
+++ src/main/java/com/elara/app/unit_of_measure_service/model/UomStatus.java
@@ -0,0 +1,37 @@
+package com.elara.app.unit_of_measure_service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity(name = "uom_status")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UomStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE) // Prevents modification of the ID after creation
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(max = 50, message = "Name cannot exceed 50 characters")
+    @Column(name = "name", unique = true, nullable = false, length = 50)
+    private String name;
+
+    @Size(max = 200, message = "Description cannot exceed 200 characters")
+    @Column(name = "description", length = 200)
+    private String description;
+
+    @Builder.Default
+    @NotNull(message = "Is usable cannot be null")
+    @Column(name = "is_usable", nullable = false)
+    private Boolean isUsable = true;
+
+}
