db2f6552bd0f69b64ad4e9e15f8b302f05d6a9f8
Andrés Bermúdez <91708539+andresbetov@users.noreply.github.com>
2025-07-06 21:03:29 -0500

    feat(tests): activate test profile for UnitOfMeasureServiceApplicationTests

--- src/test/java/com/elara/app/unit_of_measure_service/UnitOfMeasureServiceApplicationTests.java
+++ src/test/java/com/elara/app/unit_of_measure_service/UnitOfMeasureServiceApplicationTests.java
@@ -2,12 +2,14 @@ package com.elara.app.unit_of_measure_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UnitOfMeasureServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() { // This test will pass if the application context loads successfully
 	}
 
 }
