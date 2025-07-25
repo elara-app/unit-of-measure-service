71176a18fd03fc05bcd02344d52aceb1340cdf75
Julian Bermudez <julianbetov@gmail.com>
2025-07-24 22:58:41 -0500

    feat(errors): enhance BaseException to support localized messages with MessageService

--- src/main/java/com/elara/app/unit_of_measure_service/exceptions/BaseException.java
+++ src/main/java/com/elara/app/unit_of_measure_service/exceptions/BaseException.java
@@ -1,25 +1,37 @@
 package com.elara.app.unit_of_measure_service.exceptions;
 
+import com.elara.app.unit_of_measure_service.utils.ApplicationContextHolder;
 import com.elara.app.unit_of_measure_service.utils.ErrorCode;
+import com.elara.app.unit_of_measure_service.utils.MessageService;
 import lombok.Getter;
 
 @Getter
 public class BaseException extends RuntimeException {
+
     private final int code;
     private final String value;
     private final String message;
 
-    public BaseException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
+    public BaseException(ErrorCode errorCode, Object... args) {
         this.code = errorCode.getCode();
         this.value = errorCode.getValue();
-        this.message = errorCode.getMessage();
+        this.message = getLocalizeMessage(errorCode, args);
     }
 
-    public BaseException(ErrorCode errorCode, String customMessage) {
-        super(customMessage != null ? errorCode.getMessage() + ": " + customMessage : errorCode.getMessage());
+    public BaseException(ErrorCode errorCode, String customMessage, Object... args) {
+        String baseMessage = getLocalizeMessage(errorCode, args);
         this.code = errorCode.getCode();
         this.value = errorCode.getValue();
-        this.message = customMessage != null ? errorCode.getMessage() + ": " + customMessage : errorCode.getMessage();
+        this.message = customMessage != null ? baseMessage + ": " + customMessage : baseMessage;
     }
+
+    private String getLocalizeMessage(ErrorCode errorCode, Object... args) {
+        try {
+            MessageService messageService = ApplicationContextHolder.getBean(MessageService.class);
+            return messageService.getMessage(errorCode, args);
+        } catch (Exception e) {
+            return errorCode.getValue();
+        }
+    }
+
 }
