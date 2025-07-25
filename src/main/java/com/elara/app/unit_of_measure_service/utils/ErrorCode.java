f36d9363f6ca0ecf6e538dbed5541854b38639a9
Julian Bermudez <julianbetov@gmail.com>
2025-07-24 22:09:39 -0500

    feat(errors): update error messages to use localization keys

--- src/main/java/com/elara/app/unit_of_measure_service/utils/ErrorCode.java
+++ src/main/java/com/elara/app/unit_of_measure_service/utils/ErrorCode.java
@@ -7,15 +7,15 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    DATABASE_ERROR(1001, "DATABASE_ERROR", "A database error occurred"),
-    INVALID_DATA(1002, "INVALID_DATA", "Provided data is invalid"),
-    RESOURCE_CONFLICT(1003, "RESOURCE_CONFLICT", "Resource conflict occurred"),
-    RESOURCE_NOT_FOUND(1004, "RESOURCE_NOT_FOUND", "Resource not found"),
-    SERVICE_UNAVAILABLE(1005, "SERVICE_UNAVAILABLE", "Service is currently unavailable"),
-    UNEXPECTED_ERROR(1006, "UNEXPECTED_ERROR", "An unexpected error occurred");
+    DATABASE_ERROR(1001, "DATABASE_ERROR", "app.error.database"),
+    INVALID_DATA(1002, "INVALID_DATA", "uom.error.invalid.data"),
+    RESOURCE_CONFLICT(1003, "RESOURCE_CONFLICT", "uom.error.resource.conflict"),
+    RESOURCE_NOT_FOUND(1004, "RESOURCE_NOT_FOUND", "uom.error.resource.not.found"),
+    SERVICE_UNAVAILABLE(1005, "SERVICE_UNAVAILABLE", "app.error.service.unavailable"),
+    UNEXPECTED_ERROR(1006, "UNEXPECTED_ERROR", "app.error.unexpected");
 
     private final int code;
     private final String value;
-    private final String message;
+    private final String key;
 
 }
