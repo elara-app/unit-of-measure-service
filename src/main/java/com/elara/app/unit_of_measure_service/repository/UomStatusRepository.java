465c4bf315bf7c0a7d28a85c41d688da386e9b39
Julian Bermudez <julianbetov@gmail.com>
2025-07-28 16:22:29 -0500

    feat(repository): add methods to check existence by name and update status

--- src/main/java/com/elara/app/unit_of_measure_service/repository/UomStatusRepository.java
+++ src/main/java/com/elara/app/unit_of_measure_service/repository/UomStatusRepository.java
@@ -4,6 +4,9 @@ import com.elara.app.unit_of_measure_service.model.UomStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -16,5 +19,11 @@ public interface UomStatusRepository extends JpaRepository<UomStatus, Long> {
     Page<UomStatus> findAllByNameContainingIgnoreCase(String name, Pageable pageable);
 
     Page<UomStatus> findAllByIsUsable(Boolean isUsable, Pageable pageable);
+
+    Boolean existsByName(String name);
+
+    @Modifying
+    @Query("UPDATE uom_status u SET u.isUsable = :isUsable WHERE u.id = :id")
+    void changeStatus(@Param("id") Long id, @Param("isUsable") Boolean isUsable);
 
 }
