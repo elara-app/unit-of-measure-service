0b8314123be924f7d3021c4c284367478de0c83c
Andrés Bermúdez <91708539+andresbetov@users.noreply.github.com>
2025-08-08 10:52:27 -0500

    refactor(service): remove unused import in UomStatusService interface

--- src/main/java/com/elara/app/unit_of_measure_service/service/interfaces/UomStatusService.java
+++ src/main/java/com/elara/app/unit_of_measure_service/service/interfaces/UomStatusService.java
@@ -6,8 +6,6 @@ import com.elara.app.unit_of_measure_service.dto.update.UomStatusUpdate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
-
 public interface UomStatusService {
 
     UomStatusResponse save(UomStatusRequest request);
