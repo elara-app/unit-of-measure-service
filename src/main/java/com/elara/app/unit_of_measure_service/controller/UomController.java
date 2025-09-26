14c937e5bd8cee2c18809ecfd22becf5748a5f2f
Julian Bermudez <julianbetov@gmail.com>
2025-09-26 14:27:17 -0500

    feat(controller): add updateUom method to UomController for updating UOM entries

--- src/main/java/com/elara/app/unit_of_measure_service/controller/UomController.java
+++ src/main/java/com/elara/app/unit_of_measure_service/controller/UomController.java
@@ -2,6 +2,8 @@ package com.elara.app.unit_of_measure_service.controller;
 
 import com.elara.app.unit_of_measure_service.dto.request.UomRequest;
 import com.elara.app.unit_of_measure_service.dto.response.UomResponse;
+import com.elara.app.unit_of_measure_service.dto.response.UomStatusResponse;
+import com.elara.app.unit_of_measure_service.dto.update.UomUpdate;
 import com.elara.app.unit_of_measure_service.service.interfaces.UomService;
 import com.elara.app.unit_of_measure_service.service.interfaces.UomStatusService;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,10 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/uom", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -42,6 +41,14 @@ public class UomController {
         System.out.println(response);
         log.info("[Uom-controller-service] Uom created with id: {}.", response.id());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UomResponse> updateUom(@PathVariable Long id, @Valid @RequestBody UomUpdate update) {
+        log.info("[updateUom] Request to update Uom id: {} with data: {}", id, update);
+        UomResponse response = service.update(id, update);
+        log.info("[updateUom] Uom updated: {}", response);
+        return ResponseEntity.ok(response);
     }
 
 }
