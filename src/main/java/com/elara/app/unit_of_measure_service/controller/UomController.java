a0ea0bc4808bf29999404a6244a20596c628f573
Andrés Bermúdez <91708539+andresbetov@users.noreply.github.com>
2025-09-25 21:13:24 -0500

    feat(controller): add UomController for managing unit of measure creation

---
+++ src/main/java/com/elara/app/unit_of_measure_service/controller/UomController.java
@@ -0,0 +1,47 @@
+package com.elara.app.unit_of_measure_service.controller;
+
+import com.elara.app.unit_of_measure_service.dto.request.UomRequest;
+import com.elara.app.unit_of_measure_service.dto.response.UomResponse;
+import com.elara.app.unit_of_measure_service.service.interfaces.UomService;
+import com.elara.app.unit_of_measure_service.service.interfaces.UomStatusService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jdk.swing.interop.SwingInterOpUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/uom", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+@Validated
+@Slf4j
+@Tag(
+    name = "Uom Management",
+    description = ""
+)
+public class UomController {
+
+    private final UomService service;
+
+    // ========================================
+    // CREATE OPERATIONS
+    // ========================================
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UomResponse> createUom(@Valid @RequestBody UomRequest request) {
+        log.info("[Uom-controller-create] Request to create Uom: {}.", request);
+        UomResponse response = service.save(request);
+        System.out.println(response);
+        log.info("[Uom-controller-service] Uom created with id: {}.", response.id());
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+}
