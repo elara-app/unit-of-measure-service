836c292c3546756f4f265ee2f06bcaff503d5ec1
Julian Bermudez <julianbetov@gmail.com>
2025-10-12 20:24:42 -0500

    refactor(error): update RESOURCE_CONFLICT message key for improved clarity

--- src/main/java/com/elara/app/unit_of_measure_service/utils/ErrorCode.java
+++ src/main/java/com/elara/app/unit_of_measure_service/utils/ErrorCode.java
@@ -9,7 +9,7 @@ public enum ErrorCode {
 
     DATABASE_ERROR(1001, "DATABASE_ERROR", "global.error.database"),
     INVALID_DATA(1002, "INVALID_DATA", "uom.invalid.data"),
-    RESOURCE_CONFLICT(1003, "RESOURCE_CONFLICT", "global.error.conflict"),
+    RESOURCE_CONFLICT(1003, "RESOURCE_CONFLICT", "crud.already.exists"),
     RESOURCE_NOT_FOUND(1004, "RESOURCE_NOT_FOUND", "global.error.not.found"),
     SERVICE_UNAVAILABLE(1005, "SERVICE_UNAVAILABLE", "global.error.service.unavailable"),
     UNEXPECTED_ERROR(1006, "UNEXPECTED_ERROR", "global.error.unexpected");
