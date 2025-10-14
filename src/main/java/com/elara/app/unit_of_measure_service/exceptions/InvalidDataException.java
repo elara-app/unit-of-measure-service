a0f5d195de6fd4dbdd442fac228f0bc6e0de2c75
Andrés Bermúdez <91708539+andresbetov@users.noreply.github.com>
2025-10-13 20:12:14 -0500

    refactor(exceptions): remove custom message constructors from exception classes

--- src/main/java/com/elara/app/unit_of_measure_service/exceptions/InvalidDataException.java
+++ src/main/java/com/elara/app/unit_of_measure_service/exceptions/InvalidDataException.java
@@ -6,8 +6,4 @@ public class InvalidDataException extends BaseException {
     public InvalidDataException(Object... args) {
         super(ErrorCode.INVALID_DATA, args);
     }
-
-    public InvalidDataException(String customMessage, Object... args) {
-        super(ErrorCode.INVALID_DATA, customMessage, args);
-    }
 }
