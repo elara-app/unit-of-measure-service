1f0dbd4770514fadb3cf441f22335084dba46eae
Andrés Bermúdez <91708539+andresbetov@users.noreply.github.com>
2026-03-27 18:33:10 -0500

    refactor(exceptions): update custom exception constructors to accept message strings

--- src/main/java/com/elara/app/unit_of_measure_service/exceptions/BaseException.java
+++ src/main/java/com/elara/app/unit_of_measure_service/exceptions/BaseException.java
@@ -12,19 +12,10 @@ public class BaseException extends RuntimeException {
     private final String value;
     private final String message;
 
-    public BaseException(ErrorCode errorCode, Object... args) {
+    public BaseException(ErrorCode errorCode, String message) {
         this.code = errorCode.getCode();
         this.value = errorCode.getValue();
-        this.message = getLocalizeMessage(errorCode, args);
-    }
-
-    private String getLocalizeMessage(ErrorCode errorCode, Object... args) {
-        try {
-            MessageService messageService = ApplicationContextHolder.getBean(MessageService.class);
-            return messageService.getMessage(errorCode, args);
-        } catch (Exception e) {
-            return errorCode.getValue();
-        }
+        this.message = message;
     }
 
 }
